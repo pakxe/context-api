d708c5b3f22b6568ec85934772e78bc2783e0952
pakxe <pigkill40@naver.com>
2022-08-02 17:49:48 +0900

    버튼 누르면 커지는 부모상자 완성본

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import React, { useRef, useEffect, useState } from "react";
 
 const Background = styled(motion.div)`
-  background: linear-gradient(45deg, #84fab0, #8fd3f4);
+  background: linear-gradient(45deg, #2af598, #009efd);
   width: 100vw;
   height: 100vh;
   display: flex;
@@ -11,45 +11,41 @@ const Background = styled(motion.div)`
   align-items: center;
 `;
 
-const Switch = styled(motion.div)`
-  width: 160px;
-  border-radius: 50px;
-  background-color: rgba(255, 255, 255, 0.4);
-  cursor: pointer;
+const Parent = styled(motion.div)`
+  width: 100px;
+  height: 100px;
   display: flex;
-  justify-content: flex-start;
-  padding: 10px;
+  justify-content: center;
+  align-items: center;
+  background-color: white;
 
-  &[data-isOn="true"] {
-    justify-content: flex-end;
+  &[data-isOpen="true"] {
+    width: 400px;
+    height: 200px;
   }
 `;
-//대괄호 안에 넣어준 것은 속성(특성) 선택자임
 
-const Handle = styled(motion.div)`
-  height: 80px;
-  width: 80px;
-  border-radius: 40px;
-  background-color: white;
+const Child = styled(motion.div)`
+  width: 40px;
+  height: 40px;
+  background: #f107a3;
+  border-radius: 50%;
 `;
-
 function App() {
-  const [isOn, setIsOn] = useState(false);
-
-  const toggleSwitch = () => setIsOn(!isOn);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <Background>
-      <Switch data-isOn={isOn} onClick={toggleSwitch}>
-        <Handle layout transition={spring} />
-      </Switch>
+      <Parent
+        layout
+        data-isOpen={isOpen}
+        onClick={() => setIsOpen(!isOpen)}
+        initial={{ borderRadius: 50 }}
+      >
+        <Child layout />
+      </Parent>
     </Background>
   );
 }
 
-const spring = {
-  stiffness: 700,
-  type: "spring",
-  damping: 30,
-};
 export default App;
