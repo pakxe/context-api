89363ead3e88036078fa0ec19438e031e562f2de
pakxe <pigkill40@naver.com>
2022-08-03 14:54:15 +0900

    두 상자를 넘나드는 공(layoutId 적용) 완성본

--- src/App.js
+++ src/App.js
@@ -7,83 +7,50 @@ import {
 import styled from "styled-components";
 import React, { useRef, useEffect, useState } from "react";
 import "./style.css";
-
+import { toHaveAccessibleDescription } from "@testing-library/jest-dom/dist/matchers";
 const Background = styled(motion.div)`
-  background: linear-gradient(45deg, #df89b5, #bfd9fe);
+  background: linear-gradient(45deg, #ed6ea0, #ec8c69);
   width: 100vw;
   height: 100vh;
   display: flex;
-  justify-content: center;
+  justify-content: space-evenly;
   align-items: center;
 `;
 
 const Box = styled(motion.div)`
-  width: 400px;
+  width: 200px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
-  position: absolute;
-  top: 150px;
+  border-radius: 20px;
+  box-shadow: 0 2px 3px 2px rgba(0, 0, 0, 0.2);
+  background: white;
   display: flex;
   justify-content: center;
   align-items: center;
-  font-size: 28px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
+const Circle = styled(motion.div)`
+  height: 40px;
+  width: 40px;
+  background: #9face6;
+  box-shadow: 0 2px 2px 1px rgba(0, 0, 0, 0.1);
+`;
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [back, setBack] = useState(false); // 슬라이드가 뒤로가는지 확인하는 state
-  const nextPlease = () => {
-    setBack(false);
-    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-  };
-  const prevPlease = () => {
-    setBack(true);
-    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-  };
+  const [clicked, setClicked] = useState(false);
 
-  const boxVariants = {
-    // 인수인 isBack은 Box의 custom={back}으로 받은 값임.
-    entry: (isBack) => {
-      return { opacity: 0.2, scale: 0.5, x: isBack ? -500 : 500 };
-    },
-    visible: { opacity: 1, scale: 1, x: 0, transition: { duration: 0.3 } },
-    exit: (isBack) => {
-      return {
-        opacity: 0.2,
-        scale: 0.5,
-        x: isBack ? 500 : -500,
-        transition: { duration: 0.3 },
-      };
-    },
-  };
   return (
-    <Background>
-      {/* 
-        AnimatePresence에도 custom 작성,
-        exitBeforeEnter : 전 요소의 animation이 완전히 끝난 후 다음 animation이 작동 함. ex) <AnimatePresence exitBeforeEnter custom={back},
-        onExitComplete: exit가 끝날 때 실행. >
-      */}
-      <AnimatePresence onExitComplete custom={back}>
-        <Box
-          custom={back} // back에 따라 boxVariants가 달라짐.
-          key={visible} // React js의 children은 각 고유의 key가 필요하기 때문에 key값이 변하면 다른 요소로 변함.
-          variants={boxVariants}
-          initial="entry"
-          animate="visible"
-          exit="exit"
-        >
-          {visible}
-        </Box>
-      </AnimatePresence>
-      <button onClick={nextPlease} style={{ fontSize: "30px" }}>
-        next
-      </button>
-      <button onClick={prevPlease} style={{ fontSize: "30px" }}>
-        prev
-      </button>
+    <Background onClick={() => setClicked(!clicked)}>
+      <Box>
+        {clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 0 }}></Circle>
+        ) : null}
+      </Box>
+      <Box>
+        {!clicked ? (
+          <Circle layoutId="circle" style={{ borderRadius: 50 }}></Circle>
+        ) : null}
+      </Box>
     </Background>
   );
 }
+
 export default App;
