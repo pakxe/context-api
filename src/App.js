48d77b9039129b78379543f2471809a5223a2555
pakxe <pigkill40@naver.com>
2022-08-04 21:18:42 +0900

    누르면 가운데로 와서 콘텐츠 보여주는 상자들

--- src/App.js
+++ src/App.js
@@ -18,7 +18,6 @@ const Background = styled(motion.div)`
 `;
 
 const Box = styled(motion.div)`
-  width: 200px;
   height: 200px;
   border-radius: 20px;
   box-shadow: 0 2px 3px 2px rgba(0, 0, 0, 0.2);
@@ -26,29 +25,62 @@ const Box = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
+
+  cursor: pointer;
 `;
 
-const Circle = styled(motion.div)`
-  height: 40px;
-  width: 40px;
-  background: #9face6;
-  box-shadow: 0 2px 2px 1px rgba(0, 0, 0, 0.1);
+const Grid = styled(motion.div)`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 70vw;
+
+  grid-gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+//grid-column: span 2 => 해당하는 요소는 열의 2개만큼의 공간을 차지하라는 뜻
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 function App() {
-  const [clicked, setClicked] = useState(false);
+  const [clickedId, setClickedId] = useState(null);
 
   return (
-    <Background onClick={() => setClicked(!clicked)}>
-      <Box>
-        {clicked ? (
-          <Circle layoutId="circle" style={{ borderRadius: 0 }}></Circle>
+    <Background>
+      <Grid>
+        {["1", "2", "3", "4"].map((n) => (
+          <Box key={n} layoutId={n} onClick={() => setClickedId(n)}></Box>
+        ))}
+      </Grid>
+      <AnimatePresence>
+        {clickedId ? (
+          <Overlay
+            onClick={() => {
+              setClickedId(null);
+            }}
+            initial={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+            animate={{ backgroundColor: "rgba(0, 0, 0, .5)" }}
+            exit={{ backgroundColor: "rgba(0, 0, 0, 0)" }}
+          >
+            <Box
+              layoutId={clickedId}
+              style={{
+                width: 400,
+                height: 200,
+                backgroundColor: "rgb(0, 208, 162)",
+              }}
+            ></Box>
+          </Overlay>
         ) : null}
-      </Box>
-      <Box>
-        {!clicked ? (
-          <Circle layoutId="circle" style={{ borderRadius: 50 }}></Circle>
-        ) : null}
-      </Box>
+      </AnimatePresence>
     </Background>
   );
 }
